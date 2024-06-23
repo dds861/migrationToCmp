058b6a855c5c1d5cb33558c0b9c6b84604809d56
Daniyar Nurgaliyev <dds861@gmail.com>
2024-06-23 18:57:51 +0200

    Integrate Room database with KMP

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,12 @@
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.androidx.room)
 }
 
 kotlin {
@@ -12,7 +17,10 @@ kotlin {
             }
         }
     }
-    
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+        languageVersion.set(KOTLIN_1_9)
+    }
     listOf(
         iosX64(),
         iosArm64(),
@@ -30,9 +38,18 @@ kotlin {
             // koin dependencies
             implementation(project.dependencies.platform(libs.koin.bom))
             implementation(libs.koin.core)
+            // room dependencies
+            implementation(libs.androidx.room.runtime)
+            implementation(libs.androidx.sqlite.bundled)
+            implementation(libs.androidx.sqlite)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+
+        androidMain.dependencies {
+            // viewmodel compose dependencies
+            implementation(libs.androidx.lifecycle.viewmodel.compose)
         }
     }
 }
@@ -47,4 +64,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+dependencies {
+    add("kspAndroid", libs.androidx.room.compiler)
+    add("kspIosSimulatorArm64", libs.androidx.room.compiler)
+    add("kspIosX64", libs.androidx.room.compiler)
+    add("kspIosArm64", libs.androidx.room.compiler)
+}
+
+room {
+    schemaDirectory("$projectDir/schemas")
 }
